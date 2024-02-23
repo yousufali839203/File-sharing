509a2bec3dd568ac46c5a1709f001482bbeb2e4b
Yousuf <yousufali06296@gmail.com>
2024-02-23 12:09:58 +0530

    m

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,9 +1,9 @@
 import axios from "axios";
-const API_URL = "";
+const API_URL = "https://naughty-cloak.cyclic.app";
 
 export const uploadFile = async (data) => {
   try {
-    let response = await axios.post(`/upload`, data);
+    let response = await axios.post(`${API_URL}/upload`, data);
     return response.data;
   } catch (error) {
     console.error("Error while calling api " + error.message);
